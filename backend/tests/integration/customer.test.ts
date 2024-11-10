b2ccf06ac57c59074f3b38a28aecbb1aaedf57cc
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2024-11-10 18:31:27 -0300

    test: add integration test to GET `/customers` endpoint

--- backend/tests/integration/customer.test.ts
+++ backend/tests/integration/customer.test.ts
@@ -33,3 +33,33 @@ describe('POST to /customers', () => {
     });
   });
 });
+
+describe('GET to /customers', () => {
+  it('should return 200 and a message when fetching customers', async () => {
+    const response = await request(app).get('/customers');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('message');
+    expect(response.body).toHaveProperty('data');
+    expect(response.body.data).toHaveLength(1);
+    expect(response.body.data[0]).toHaveProperty('id');
+    expect(response.body.data[0]).toHaveProperty('name');
+    expect(response.body.data[0]).toHaveProperty('email');
+    expect(response.body.data[0]).toHaveProperty('cpf');
+    expect(response.body.data[0]).toHaveProperty('phone');
+    expect(response.body.data[0]).toHaveProperty('status');
+    expect(response.body).toStrictEqual({
+      message: 'Customers fetched successfully',
+      data: [
+        {
+          id: 1,
+          name: 'John Doe',
+          email: 'john@mail.com',
+          cpf: '123.456.789-11',
+          phone: '1234567891',
+          status: 'active',
+        },
+      ],
+    });
+  });
+});
